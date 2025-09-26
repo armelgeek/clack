c11f3e7b9e92be7823bb720cd784b23515a9eaba
Hajavololona Armellin <armelgeek5@gmail.com>
2025-09-26 09:24:47 +0300

    Refactor database layer to use vapostore-db package

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,14 +1,2 @@
-import { config } from 'dotenv'
-import { drizzle } from 'drizzle-orm/postgres-js'
-import * as schema from './schema'
-
-const postgres = require('postgres')
-
-config()
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is not set')
-}
-
-export const client = postgres(process.env.DATABASE_URL)
-export const db = drizzle(client, { schema })
+export { db, client } from 'vapostore-db';
+export * as schema from 'vapostore-db';
